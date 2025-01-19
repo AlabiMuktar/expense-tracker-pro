a14ab767301f5184567f0e9c393e3bf148b72f22
Muktar <muktarayodeji07@gmail.com>
2025-01-19 23:28:54 +0100

    fix: cors package and fallback error page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("express-async-errors");
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors')
 require("dotenv").config();
 
 const errorHandler = require("./handlers/errorHandler");
@@ -23,12 +24,20 @@ require("./models/users.model")
 require("./models/transactions.model")
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 const PORT = 8000;
 
 app.use('/api/users', usersRoutes)
 app.use('/api/transactions', transactionRoutes)
+
+app.all('*', (req, res)=> {
+  res.status(404).json({
+    status: "failed",
+    message: "Not found"
+  })
+})
 
 app.use(errorHandler);
 
